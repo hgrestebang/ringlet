7b48bd5793531f0643216376299c2788b5091a18
jotaj90 <jotaj90@hotmailcom>
2014-01-06 09:44:29 -0600

    working on listing

--- ringlet/www/js/angular/controllers.js
+++ ringlet/www/js/angular/controllers.js
@@ -6,4 +6,14 @@ function UserCtrl($scope, DAO, $compile, $timeout){
     var appConfig = {serverHost:'192.168.0.101', appName:'ringlet', token:''};
 
 
+    //-----------------------------Listings functions-------------------------------------------------
+    $scope.getRinglets = function(ringlets){
+        $scope.ringlets = ringlets;
+
+
+    };
+    $scope.refreshListingImages = function(){
+        $('#listing-lImages').listview('refresh');
+        return true;
+    }
 }
